4e52c1441786f4d489890aa604995935bf476714
goxny <xiaoze3300@163.com>
2025-06-06 23:07:17 +0800

    完成main.go

--- cmd/XuanXiang/main.go
+++ cmd/XuanXiang/main.go
@@ -6,14 +6,15 @@ import (
 
 	"XuanXiang/internal/conf"
 
+	zerolog_kratos "github.com/go-kratos/kratos/contrib/log/zerolog/v2"
 	"github.com/go-kratos/kratos/v2"
 	"github.com/go-kratos/kratos/v2/config"
 	"github.com/go-kratos/kratos/v2/config/file"
-	zerolog "github.com/go-kratos/kratos/v2/contrib/log/zerolog/v2"
 	"github.com/go-kratos/kratos/v2/log"
 	"github.com/go-kratos/kratos/v2/middleware/tracing"
 	"github.com/go-kratos/kratos/v2/transport/grpc"
 	"github.com/go-kratos/kratos/v2/transport/http"
+	"github.com/rs/zerolog"
 
 	_ "go.uber.org/automaxprocs"
 )
@@ -51,16 +52,16 @@ func newApp(logger log.Logger, gs *grpc.Server, hs *http.Server) *kratos.App {
 
 func main() {
 	flag.Parse() //解析命令行参数
-	// logger := zerolog.NewLogger()
-	logger := log.With(zerolog.NewLogger(os.Stdout), //with：日志伴随的其他信息	//将输出记录Stout到新的日志记录器
-		"ts", log.DefaultTimestamp, //添加时间戳
-		"caller", log.DefaultCaller, //添加调用者信息
-		"service.id", id, //添加服务ID
-		"service.name", Name, //添加服务名称
-		"service.version", Version, //添加服务版本
-		"trace.id", tracing.TraceID(), //添加traceID
-		"span.id", tracing.SpanID(), //添加spanID
-	)
+
+	// logger := log.With(zerolog.NewLogger(os.Stdout), //with：日志伴随的其他信息	//将输出记录Stout到新的日志记录器
+	// 	"ts", log.DefaultTimestamp, //添加时间戳
+	// 	"caller", log.DefaultCaller, //添加调用者信息
+	// 	"service.id", id, //添加服务ID
+	// 	"service.name", Name, //添加服务名称
+	// 	"service.version", Version, //添加服务版本
+	// 	"trace.id", tracing.TraceID(), //添加traceID
+	// 	"span.id", tracing.SpanID(), //添加spanID
+	// )
 
 	c := config.New(
 		config.WithSource(
@@ -77,6 +78,25 @@ func main() {
 	if err := c.Scan(&bc); err != nil {
 		panic(err)
 	}
+
+	level, err := zerolog.ParseLevel(bc.LogLevel)
+	if err != nil {
+		panic(err)
+	}
+
+	// zerolog.TimeFieldFormat = "2006-01-02 15:04:05"
+	zerolog.SetGlobalLevel(level)
+
+	Zerologger := zerolog.New(os.Stdout) //新建一个zerolog的记录器，类型为zerolog.Logger
+	logger := zerolog_kratos.NewLogger(&Zerologger)
+	logger = log.With(logger, //with：日志伴随的其他信息	//kratos的logger可以接受任意类型的logger
+		"ts", log.DefaultTimestamp, //添加时间戳
+		"service.id", id, //添加服务ID
+		"service.name", Name, //添加服务名称
+		"service.version", Version, //添加服务版本
+		"trace.id", tracing.TraceID(), //添加traceID
+		"span.id", tracing.SpanID(), //添加spanID
+	)
 
 	app, cleanup, err := wireApp(bc.Server, bc.Data, logger)
 	if err != nil {
--- go.mod
+++ go.mod
@@ -1,12 +1,12 @@
-module XuanXiang
+module github.com/SoLikeWind/XuanXiang
 
-go 1.23.0
-
-toolchain go1.24.1
+go 1.24.1
 
 require (
+	github.com/go-kratos/kratos/contrib/log/zerolog/v2 v2.0.0-20250527152916-d6f5f00cf562
 	github.com/go-kratos/kratos/v2 v2.8.4
 	github.com/google/wire v0.6.0
+	github.com/rs/zerolog v1.34.0
 	go.uber.org/automaxprocs v1.5.1
 	google.golang.org/genproto/googleapis/api v0.0.0-20240528184218-531527333157
 	google.golang.org/grpc v1.65.0
@@ -25,7 +25,6 @@ require (
 	github.com/kr/text v0.2.0 // indirect
 	github.com/mattn/go-colorable v0.1.14 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
-	github.com/rs/zerolog v1.34.0 // indirect
 	go.opentelemetry.io/otel v1.24.0 // indirect
 	go.opentelemetry.io/otel/metric v1.24.0 // indirect
 	go.opentelemetry.io/otel/trace v1.24.0 // indirect
